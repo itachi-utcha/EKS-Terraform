404081c7954d15a37b183997cd85314f25198d17
Itachi <144083294+itachi-utcha@users.noreply.github.com>
2024-04-10 16:52:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "terraform-state-bucket-122" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.state"
+    region = "us-east-1"
   }
 }
